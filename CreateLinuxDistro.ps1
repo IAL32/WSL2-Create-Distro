bac768254f33e71546712e7a75f32f1f1196b37c
IAL32 <adrian.d.castro.t@gmail.com>
2020-12-04 22:49:30 +0100

    Automatically set default shell to bash, and set password for user and root

--- CreateLinuxDistro.ps1
+++ CreateLinuxDistro.ps1
@@ -71,11 +71,21 @@ Param (
     [ValidateNotNullOrEmpty()]
     $OUTPUT_DISTRONAME,
 
+    # The default shell
+    [Parameter(
+        Mandatory = $false,
+        ValueFromPipeline = $false,
+        ValueFromRemainingArguments = $false)]
+    [ValidateNotNull()]
+    [ValidateNotNullOrEmpty()]
+    $DEFAULT_SHELL = "/bin/bash",
+
     # Whether or not to create a user along with the distribution
     [Parameter(
         Mandatory = $false,
         ValueFromPipeline = $false,
-        ValueFromRemainingArguments = $false
+        ValueFromRemainingArguments = $false,
+        ParameterSetName='USER'
     )]
     [bool]
     $CREATE_USER = $false,
@@ -84,18 +94,31 @@ Param (
     [Parameter(
         Mandatory = $false,
         ValueFromPipeline = $false,
-        ValueFromRemainingArguments = $false
+        ValueFromRemainingArguments = $false,
+        ParameterSetName='USER'
     )]
     [ValidateNotNull()]
     [ValidateNotNullOrEmpty()]
-    [ValidatePattern("[a-zA-Z0-9]*")]
+    [ValidatePattern("[a-zA-Z0-9]+")]
     $CREATE_USER_USERNAME,
+
+    # The password for the user
+    [Parameter(
+        Mandatory = $true,
+        ValueFromPipeline = $true,
+        ValueFromRemainingArguments = $false,
+        ParameterSetName='USER'
+    )]
+    [ValidateNotNull()]
+    [ValidateNotNullOrEmpty()]
+    $CREATE_USER_PASSWORD,
 
     # Whether or not to automatically add the user to a group
     [Parameter(
         Mandatory = $false,
         ValueFromPipeline = $false,
-        ValueFromRemainingArguments = $false
+        ValueFromRemainingArguments = $false,
+        ParameterSetName='USER'
     )]
     [bool]
     $ADD_USER_TO_GROUP = $false,
@@ -104,7 +127,8 @@ Param (
     [Parameter(
         Mandatory = $false,
         ValueFromPipeline = $false,
-        ValueFromRemainingArguments = $false
+        ValueFromRemainingArguments = $false,
+        ParameterSetName='USER'
     )]
     [ValidateNotNull()]
     [ValidateNotNullOrEmpty()]
@@ -116,7 +140,16 @@ Param (
         ValueFromPipeline = $false,
         ValueFromRemainingArguments = $false
     )]
-    $SET_USER_AS_DEFAULT = "root"
+    [ValidateNotNull()]
+    $SET_USER_AS_DEFAULT = "root",
+
+    # The default password for the root user
+    [Parameter(
+        Mandatory = $true,
+        ValueFromPipeline = $true,
+        ValueFromRemainingArguments = $false
+    )]
+    $ROOT_PASSWORD = "root-password-change-me-please-234!"
 )
 
 begin {
@@ -131,14 +164,29 @@ process {
         if ($CREATE_USER) {
             Write-Output "Creating user $CREATE_USER_USERNAME"
             wsl -d $OUTPUT_DISTRONAME /usr/sbin/useradd -m $CREATE_USER_USERNAME
+            Write-Output "Setting password for user $CREATE_USER_USERNAME to the selected one"
+            # Setting password to user
+            wsl -d $OUTPUT_DISTRONAME /bin/bash -c "echo -e '$CREATE_USER_PASSWORD\n$CREATE_USER_PASSWORD' | /usr/bin/passwd $CREATE_USER_USERNAME"
+            Write-Output "Setting shell for user $CREATE_USER_USERNAME to $DEFAULT_SHELL"
+            # Setting default shell for user as bash
+            wsl -d $OUTPUT_DISTRONAME /usr/sbin/usermod --shell $DEFAULT_SHELL $CREATE_USER_USERNAME
 
             if ($ADD_USER_TO_GROUP) {
                 Write-Output "Adding user $CREATE_USER_USERNAME to group $ADD_USER_TO_GROUP_NAME"
                 wsl -d $OUTPUT_DISTRONAME /usr/sbin/adduser $CREATE_USER_USERNAME $ADD_USER_TO_GROUP_NAME
             }
         }
+
+        # Setting password to user
+        # NOTE: we have to set it BEFORE setting the default user, otherwise it will be
+        # launched with the user shell, and it will ask us for the password.
+        Write-Output "Setting default root password to the selected one"
+        wsl -d $OUTPUT_DISTRONAME /bin/bash -c "echo -e '$ROOT_PASSWORD\n$ROOT_PASSWORD' | /usr/bin/passwd"
+
+        Write-Output "Setting default user as $SET_USER_AS_DEFAULT"
         # Curtesy of https://github.com/microsoft/WSL/issues/3974#issuecomment-522921145
         Get-ItemProperty Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss\*\ DistributionName | Where-Object -Property DistributionName -eq $OUTPUT_DISTRONAME | Set-ItemProperty -Name DefaultUid -Value ((wsl -d $OUTPUT_DISTRONAME -u $SET_USER_AS_DEFAULT -e id -u) | Out-String);
+
     }
 
 }
